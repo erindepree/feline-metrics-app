d0adec26dd35b39a62d3c2900a2d50e21e1e63b4
Erin De Pree <erindepree@gmail.com>
2025-09-18 22:57:12 +0000

    set up prescriptions.py

---
+++ prescriptions.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "FelineMetrics | Mel | Prescriptions",
+    page_icon = ":material/medication:"
+)
+
+st.title(":material/pill: FelineMetrics | Mel | Prescriptions")
