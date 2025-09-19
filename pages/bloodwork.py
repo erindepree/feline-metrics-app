eb6439725e7be56198c7589dc9285bdda7171e09
Erin De Pree <erindepree@gmail.com>
2025-09-19 17:12:02 +0000

    change plot from line to scatter

--- pages/bloodwork.py
+++ pages/bloodwork.py
@@ -13,14 +13,13 @@ st.title(":material/health_metrics: Mel's Bloodwork")
 st.write("Bloodwork graphs here")
 
 df_blood = pd.read_csv("./data/mel/bloodwork.csv", parse_dates=['date'])
+df_blood.sort_values(by='date')
 
 plot_opts = df_blood.drop(columns=['date', 'comments', 'blood_parasites', 'rbc_morph', 'platelet_est']).columns
 
-# st.dataframe(df_blood)
-df_blood.sort_values(by='date')
 
 var_plot = st.radio('Select Variable to Plot:', options = plot_opts, index= 10)
 
-fig = px.line(df_blood, x='date', y=var_plot, title="Bloodwork")
+fig = px.scatter(df_blood, x='date', y=var_plot, title="Bloodwork")
 
 st.plotly_chart(fig)
