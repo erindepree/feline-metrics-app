5230c230a59c8082230096c1df488908933568fd
Erin De Pree <erindepree@gmail.com>
2025-09-19 16:22:54 +0000

    add google log in

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,21 +3,21 @@ import streamlit as st
 if "logged_in" not in st.session_state:
     st.session_state.logged_in = False
 
-def login():
-    if st.button("Log in"):
-        st.session_state.logged_in = True
-        st.rerun()
+def login_screen():
+    st.header('This app is private')
+    st.subheader("Please log in")
+    st.button("Log in with Google", on_click=st.login)
 
 def logout():
     if st.button("Log out"):
         st.session_state.logged_in = False
         st.rerun()
 
-login_page = st.Page(login, title="Log in", icon=":material/login:")
+login_page = st.Page(login_screen, title="Log in", icon=":material/login:")
 logout_page = st.Page(logout, title="Log out", icon=":material/logout:")
 
 
-dashboard = st.Page('pages/mel.py', title='Dashboard', icon=':material/dashboard:', default=True)
+dashboard = st.Page('pages/dashboard.py', title='Dashboard', icon=':material/dashboard:', default=True)
 medical=st.Page('pages/medical.py', title='Medical Encouters', icon=':material/medical_services:')
 weight = st.Page('pages/weight.py', title='Weight', icon=':material/monitor_weight:')
 vaccines = st.Page('pages/vaccines.py', title="Vaccines", icon=":material/vaccines:")
@@ -26,13 +26,18 @@ dental = st.Page('pages/dental.py', title='Dental', icon=':material/dentistry:')
 bloodwork = st.Page('pages/bloodwork.py', title="Bloodwork", icon=':material/health_metrics:')
 edit = st.Page('pages/edit.py', title='Add Information', icon=':material/edit:')
 
+
+
+
 with st.sidebar:
     st.image('images/feline_metrics_logo.png', width=200)
-#    pg.run()
-#    st.header("FelineMetrics")
 
 
-if st.session_state.logged_in:
+if not st.user.is_logged_in:
+    #login_screen()
+    pg = st.navigation([login_page])
+else:
+    st.header(f"Welcome, {st.user.name}!")
     pg = st.navigation(
         {
             "Account": [logout_page],
@@ -40,8 +45,7 @@ if st.session_state.logged_in:
             "Add Records": [edit]
         }
     )
-else:
-    pg = st.navigation([login_page])
+
 
 
 pg.run()
