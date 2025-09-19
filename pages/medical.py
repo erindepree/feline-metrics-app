ed8f853614cc78a0e2cc4f96bd4ed4ed28fd4b1c
Erin De Pree <erindepree@gmail.com>
2025-09-19 17:32:48 +0000

    show incident dataframe

--- pages/medical.py
+++ pages/medical.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 
 st.set_page_config(
     page_title = "FelineMetrics | Medical Encounters",
@@ -6,3 +7,10 @@ st.set_page_config(
 )
 
 st.title(":material/medical_services: Mel's Medical Encounters")
+
+
+df = pd.read_csv("./data/mel/incidents.csv", parse_dates=['date'])
+
+df = df.sort_values(by='date')
+
+st.dataframe(df)
