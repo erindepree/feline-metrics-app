d4aa31d160b218f28c1486e337bd43ef069cdc6f
Erin De Pree <erindepree@gmail.com>
2025-09-19 16:51:38 +0000

    add plot to bloodwork.py

--- pages/bloodwork.py
+++ pages/bloodwork.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 
 st.set_page_config(
     page_title = "FelineMetrics | Bloodwork",
@@ -12,4 +13,12 @@ st.write("Bloodwork graphs here")
 
 df_blood = pd.read_csv("./data/mel/bloodwork.csv")
 
+plot_opts = df_blood.drop(columns=['date', 'comment', 'blood_parasites', 'rbc_morph', 'platelet_est']).columns
+
 st.dataframe(df_blood)
+
+var_plot = st.radio('Select Variable to Plot:', options = plot_opts, index= 10)
+
+fig = px.line(df_blood, x='date', y=var_plot)
+
+st.plotly_chart(fig)
