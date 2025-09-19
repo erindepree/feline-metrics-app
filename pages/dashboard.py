c6bd5334dbfc28a2e117305e38d1bf9c63baa78f
Erin De Pree <erindepree@gmail.com>
2025-09-19 17:56:11 +0000

    corrected typo

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -9,11 +9,11 @@ st.title(":material/dashboard: FelineMetrics | Mel | Dashboard")
 
 st.write("Link to: weight figure, list of medical appointments, list of vaccines, dental, prescriptions, etc.")
 
-flex = st.container(horizontal=True, horizontal_alignment='center')
+row1 = st.container(horizontal=True, horizontal_alignment='center')
 
-flex.page_link("pages/medical.py", label="Medical Encounters", icon= ":material/local_hospital:")
-flex.page_link("pages/weight.py", label="Medical Encounters", icon= ":material/monitor_weight:")
-flex.page_link("pages/vaccines.py", label="Vaccines", icon= ":material/vaccines:")
-flex.page_link("pages/prescriptions.py", label="Prescriptions", icon= ":material/medication:")
-flex.page_link("pages/dental.py", label="Dental Care", icon= ":material/denistry:")
-flex.page_link("pages/bloodwork.py", label="Bloodwork Results", icon= ":material/health_metrics:")
+row1.page_link("pages/medical.py", label="Medical Encounters", icon= ":material/local_hospital:")
+row1.page_link("pages/weight.py", label="Medical Encounters", icon= ":material/monitor_weight:")
+row1.page_link("pages/vaccines.py", label="Vaccines", icon= ":material/vaccines:")
+row1.page_link("pages/prescriptions.py", label="Prescriptions", icon= ":material/medication:")
+row1.page_link("pages/dental.py", label="Dental Care", icon= ":material/dentistry:")
+row1.page_link("pages/bloodwork.py", label="Bloodwork Results", icon= ":material/health_metrics:")
